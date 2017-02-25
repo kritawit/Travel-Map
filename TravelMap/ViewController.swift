7bb2f7e6b38470db1bed70e9115355150c0abc0a
Header-Develop <kritawitbunket@gmail.com>
2017-02-25 12:55:35 +0700

    Added Mapkit,Button,Label to View

--- TravelMap/ViewController.swift
+++ TravelMap/ViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var myMapOUTLET :MKMapView!
+    @IBOutlet weak var lblInfoOUTLET: UILabel!
+    @IBOutlet weak var btnUnwindOUTLET: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
+    
+    
 
 
 }
